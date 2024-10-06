e805e1d66fbd023b94b53d2089b70236e2478f99
Yuki Minami <107658612+YUKI-South@users.noreply.github.com>
2024-10-06 22:13:15 +0900

    Add a base.html

--- workout_tracker/tracker/models.py
+++ workout_tracker/tracker/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+GENDER = (
+    ("male", "Male"),
+    ("female", "Female"),
+    ("other", "Other")
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER, null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', null=True)
+
+
+
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    exercise_type = models.CharField(max_length=100)
+    weight = models.FloatField(null=True, blank=True)
+    reps = models.PositiveIntegerField(null=True, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.exercise_type} on {self.date}'
