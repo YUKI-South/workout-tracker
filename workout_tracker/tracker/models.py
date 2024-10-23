8743169e0e6a9c9fe6c1ddeb8f7186c9aa8e2223
Yuki Minami <107658612+YUKI-South@users.noreply.github.com>
2024-10-23 12:09:42 +0900

    Add a sign-up-form class and update-form class in forms.py

--- workout_tracker/tracker/models.py
+++ workout_tracker/tracker/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 GENDER = (
     ("male", "Male"),
@@ -7,13 +7,15 @@ GENDER = (
     ("other", "Other")
 )
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER, null=True, blank=True)
     weight = models.FloatField(null=True, blank=True)
     height = models.FloatField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profiles/', null=True)
+
+    def __str__(self):
+        return self.username
 
 
 
